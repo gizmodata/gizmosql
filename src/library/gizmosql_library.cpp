c34508e16a3bc18dc037220d8c3af3be7b82b8ee
Philip Moore <prmoore77@hotmail.com>
2025-01-13 11:18:42 -0500

    WiP

--- src/library/gizmosql_library.cpp
+++ src/library/gizmosql_library.cpp
@@ -73,6 +73,7 @@ arrow::Result<std::shared_ptr<flight::sql::FlightSqlServerBase>> FlightSQLServer
                             ? flight::Location::ForGrpcTls(hostname, port)
                             : flight::Location::ForGrpcTcp(hostname, port));
 
+  std::cout << "----------------------------------------------" << std::endl;
   std::cout << "Apache Arrow version: " << ARROW_VERSION_STRING << std::endl;
 
   flight::FlightServerOptions options(location);
@@ -166,17 +167,17 @@ arrow::Result<std::shared_ptr<flight::sql::FlightSqlServerBase>> CreateFlightSQL
   // Validate the license key file
   if (license_key_filename.empty()) {
     license_key_filename = SafeGetEnvVarValue("LICENSE_KEY_FILENAME");
+  }
 
-    if (license_key_filename.empty()) {
-      return arrow::Status::Invalid("No license key file was provided - exiting.");
+  if (license_key_filename.empty()) {
+    return arrow::Status::Invalid("No license key file was provided - exiting.");
+  } else {
+    if (!fs::exists(license_key_filename)) {
+      return arrow::Status::Invalid("License key file does not exist: " +
+                                    license_key_filename);
     } else {
-      if (!fs::exists(license_key_filename)) {
-        return arrow::Status::Invalid("License key file does not exist: " +
-                                      license_key_filename);
-      } else {
-        ARROW_CHECK_OK(
-            gizmosql::LicenseFileVerifier::ValidateLicenseKey(license_key_filename));
-      }
+      ARROW_CHECK_OK(
+          gizmosql::LicenseFileVerifier::ValidateLicenseKey(license_key_filename));
     }
   }
 
@@ -288,6 +289,14 @@ int RunFlightSQLServer(const BackendType backend, fs::path &database_filename,
                        fs::path mtls_ca_cert_path, std::string init_sql_commands,
                        fs::path init_sql_commands_file, const bool &print_queries,
                        std::string license_key_filename) {
+  auto now = std::chrono::system_clock::now();
+  std::time_t currentTime = std::chrono::system_clock::to_time_t(now);
+  std::tm *localTime = std::localtime(&currentTime);
+
+  std::cout << "GizmoSQL - Copyright © " << (1900 + localTime->tm_year)
+            << " Gizmo Data LLC - All Rights Reserved" << std::endl;
+  std::cout << "----------------------------------------------" << std::endl;
+
   auto create_server_result = gizmosql::CreateFlightSQLServer(
       backend, database_filename, hostname, port, username, password, secret_key,
       tls_cert_path, tls_key_path, mtls_ca_cert_path, init_sql_commands,
